e6188cce1c36605de372d55204de30f9fe04adb8
Ellard Gerritsen <Ellard Gerritsen>
2016-04-05 14:20:25 -0400

    Assignment 2 Stuff

---
+++ projects/gerritse/dominion/refactor.c
@@ -0,0 +1,40 @@
+Refactoring:  Assignment 2
+
+
+Smithy:
+	The smithy card was refactored into SmithyCardEffect()
+
+	Change made:
+		The number of cards drawn has been reduced by 1.
+		Check the for loop.
+
+Adventurer:
+
+	The adventurer card was refactored into AdventurerCardEffect()
+	
+	Change made:
+		Silver no longer counts as a drawnTreasure and thus
+		drawntreasure count will be off.
+
+
+Feast:
+	
+	The feast card was refactored into FeastCardEffect()
+
+	Change made:
+		Feast now allows two cards to be bought. 
+		Check the while (x == 1) loop
+Great Hall:
+	
+	The great hall card was refactored into GreatHallCardEffect()
+
+	Change made:
+		Card is no longer discarded after use. 
+Steward:
+	
+	The steward card was refactored into StewardCardEffect()
+
+
+
+
+
