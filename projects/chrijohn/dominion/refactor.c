bda2b6fbb31cada9fd36b7660c9e038c562718a6
chrijohn <chrijohn@onid.oregonstate.edu>
2016-04-05 19:28:13 -0500

    Edited refractor.c

--- projects/chrijohn/dominion/refactor.c
+++ projects/chrijohn/dominion/refactor.c
@@ -1,19 +1,19 @@
 John Christiansen (Chrijohn)
 
 Smithy:
-	Only draw 2 cards instead of 3
-	Does not discard card at end
+	Player only draw 2 cards instead of 3
+	Player does not discard Smithy at end
 
 Adventurer:
-	Draws until a total of two coppers, smithy (instead of silver) or gold. 
+	Player draws until a total of two coppers, smithy (instead of silver) or gold. 
 
 Concil_Room:
-	Added +4 coins
-	Changed +1 numBuys to +4
+	Added +4 coins for player
+	Changed +1 numBuys to +4 for player
 	Removed player discarding played concil_Room card
 
 Gardens:
-	Instead of doing nothing, players get 2 actions for every card in their deck.
+	Instead of doing nothing, player get 2 actions for every card in their deck.
 	(End of game scoring not changed)
 
 Mine:
