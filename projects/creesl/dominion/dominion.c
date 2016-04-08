e5c8fd6220ef28fde662e0314018b6afc32c8a16
LCmry <crees.lauren@gmail.com>
2016-04-08 13:57:03 -0700

    Added bug to seahagEffect()

--- projects/creesl/dominion/dominion.c
+++ projects/creesl/dominion/dominion.c
@@ -1303,8 +1303,9 @@ int treasuremapEffect(int index, struct gameState *state, int currentPlayer, int
 void seahagEffect(struct gameState *state, int currentPlayer, int i)
 {
   for (i = 0; i < state->numPlayers; i++){
-    if (i != currentPlayer){
-      state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];          state->deckCount[i]--;
+    if (i == currentPlayer){
+      state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];
+      state->deckCount[i]--;
       state->discardCount[i]++;
       state->deck[i][state->deckCount[i]--] = curse;//Top card now a curse
     }
