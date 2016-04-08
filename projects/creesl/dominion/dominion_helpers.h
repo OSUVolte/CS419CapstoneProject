8ac2ab44ca91efe41e25f6a4a987680d3f4974f3
LCmry <crees.lauren@gmail.com>
2016-04-08 13:32:03 -0700

    Added refactor prototypes

--- projects/creesl/dominion/dominion_helpers.h
+++ projects/creesl/dominion/dominion_helpers.h
@@ -12,4 +12,11 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
+// Refactored cardEffect cards
+void adventurer(int drawntreasure, struct gameState *state, int currentPlayer, int z, int temphand);
+void smithy(int currentPlayer, struct *state, int handPos);
+int treasuremap(int index, struct gameState *state, int currentPlayer, int i, int handPos);
+void seahag(struct gameState *state, int currentPlayer, int i);
+void minion(struct gameState *state, int handPos, int currentPlayer, int choice1, int choice2, int i, int j);
+
 #endif
