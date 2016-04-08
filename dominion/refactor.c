79b2f609a28652561a061231cd656b966c8d9fc8
Mark Rushmere <mrushmere@gmail.com>
2016-04-08 14:39:19 -0700

    chages made to smithy and adventurer

---
+++ dominion/refactor.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+ * Mark Rushmere
+ * CS 362
+ * Assignment 2
+ * Description: Descrptions of changes made to dominion.c
+ * **********************************************************************/
+
+
+// Smithy
+
+
+
+// Adventurer
+
+
+
+
