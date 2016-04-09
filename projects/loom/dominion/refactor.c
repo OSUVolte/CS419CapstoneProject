456b59f3d6dc24f04759b2e11e3f2e0747f826e9
MikeinRealLife <michaelloo@Michaels-MacBook-Pro.local>
2016-04-08 19:00:33 -0700

    initial commit

---
+++ projects/loom/dominion/refactor.c
@@ -0,0 +1,10 @@
+//
+// Created by Michael Loo on 4/5/16.
+//
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
---
+++ projects/loom/dominion/refactor.h
@@ -0,0 +1,10 @@
+//
+// Created by Michael Loo on 4/5/16.
+//
+
+#ifndef DOMINION_REFACTOR_H
+#define DOMINION_REFACTOR_H
+#include dominion.h
+
+
+#endif //DOMINION_REFACTOR_H
