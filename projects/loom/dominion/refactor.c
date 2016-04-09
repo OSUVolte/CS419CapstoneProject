7cd8c8ededaac23daed0bed1a0a73f269d3e619b
MikeinRealLife <michaelloo@Michaels-MacBook-Pro.local>
2016-04-08 23:57:16 -0700

    redid everything?

--- projects/loom/dominion/refactor.c
+++ projects/loom/dominion/refactor.c
@@ -6,7 +6,6 @@
  * playAdventurer:
  *
  * I made a couple of tweaks that aren't super obvious
- * changed shuffle state to <= so shuffling would happen more often
  * changed it so it didn't draw the top card, it would error out and try and draw something that was out of the array
  * "forgot" to include silver card
  *
