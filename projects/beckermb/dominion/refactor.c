162745d61409ba3d415c4c1f85a24a6186dc2d4b
Ben Beckerman <beckermb@oregonstate.edu>
2016-04-09 18:00:47 -0700

    added refactor.c to describe changes

---
+++ projects/beckermb/dominion/refactor.c
@@ -0,0 +1,6 @@
+Assignment 2 Refactor:
+ * adventurer effect moved into method adventurerCardEffect
+ * smityh card effect moved into method smithyCardEffect
+ * village effect moved into method villageCardEffect
+ * great_hall effect moved into method villageCardEffect
+ * outpost effect moved into method outpostCardEffect
