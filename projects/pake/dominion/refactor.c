37e8ff38f8a8556217b5445d0056493385350756
epaknyc <eugene.a.pak@gmail.com>
2016-04-10 18:48:53 -0400

    Added Adventurer and Smithy functions

---
+++ projects/pake/dominion/refactor.c
@@ -0,0 +1,23 @@
+/*
+
+Name: Eugene Pak
+Course: CS362 Spring 2016
+Assignment 2 - refactor.c
+
+Adventurer Card:
+	pAdventurer() function to play the Adventurer card.
+	pAdventurer() takes three arguements:
+		integer handPos for hand position
+		integer currentPlayer for current player
+		struct gameState *state
+	Bug: Changed while loop for counting treasure cards from 2 to 1.
+
+Smithy Card:
+	pSmithy() function
+	pSmithy() function to play the Adventurer card.
+	pSmithy() takes three arguements:
+		integer handPos for hand position
+		integer currentPlayer for current player
+		struct gameState *state
+	Bug: Changed for loop to draw 2 cards (less than instead of less than or equal).
+*/
