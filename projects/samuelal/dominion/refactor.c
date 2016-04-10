75fe846332170277447ca823b68d8d1b4f13a272
CoderEsq <samuelal@onid.oregonstate.edu>
2016-04-10 18:36:47 -0400

    Clarified statements
    -Clarified descriptions of bugs

--- projects/samuelal/dominion/refactor.c
+++ projects/samuelal/dominion/refactor.c
@@ -2,11 +2,11 @@ Assignment 2
 
 smithy
 - Is implemented through the smithyCard() method in dominion.c . 
-BUG: When the player plays the smithy card, the player draws 4 cards from their deck.
+BUG: When the player plays the smithy card, the player draws 4 cards from their deck rather than 3.
 
 adventurer
 - Is implemented through the adventurerCard() method in dominion.c .
-BUG: Player keeps drawing cards until 3 treasure cards are drawn.
+BUG: Player keeps drawing cards until 3 treasure cards are drawn rather than 2.
 BUG: If player draws a gold card, it does not count as one of the 3 treasure cards that need to be drawn.
 
 great_hall
@@ -16,7 +16,7 @@ not discard great hall card from hand.
 
 embargo
 -Is implemented through the embargoCard() method in dominion.c .
-BUG: Card is not trashed, merely discarded.
+BUG: Card is not trashed as it should be, merely discarded.
 BUG: Card increases player's number of actions by 2 rather than number of coins.
 
 sea_hag
