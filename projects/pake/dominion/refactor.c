b6239cd0c6d69937b564cfa270c9a0b55488f398
epaknyc <eugene.a.pak@gmail.com>
2016-04-10 19:20:22 -0400

    Fixed rector.c typos

--- projects/pake/dominion/refactor.c
+++ projects/pake/dominion/refactor.c
@@ -6,16 +6,15 @@ Assignment 2 - refactor.c
 
 Adventurer Card:
 	pAdventurer() function to play the Adventurer card.
-	pAdventurer() takes three arguements:
+	pAdventurer() takes three arguments:
 		integer handPos for hand position
 		integer currentPlayer for current player
 		struct gameState *state
 	Bug: Changed while loop for counting treasure cards from 2 to 1.
 
 Smithy Card:
-	pSmithy() function
-	pSmithy() function to play the Adventurer card.
-	pSmithy() takes three arguements:
+	pSmithy() function to play the Smithy card.
+	pSmithy() takes three arguments:
 		integer handPos for hand position
 		integer currentPlayer for current player
 		struct gameState *state
