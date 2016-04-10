c45f1f4e7654bc257b17be597eaa2783ce2099e3
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2016-04-09 20:05:19 -0700

    Implemented Play-----() functions from dominion.h

--- projects/swansonb/dominion/dominion.c
+++ projects/swansonb/dominion/dominion.c
@@ -655,7 +655,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-  int drawntreasure=0;
   int cardDrawn;
   int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
@@ -667,25 +666,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+      return playAdventurer(state);
 			
     case council_room:
       //+4 Cards
@@ -829,15 +810,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+      return playSmithy(state, handPos);
 		
     case village:
       //+1 Card
@@ -1327,6 +1300,68 @@ int updateCoins(int player, struct gameState *state, int bonus)
 
   return 0;
 }
+
+int playAdventurer(struct gameState *state) {
+    int currentPlayer = whoseTurn(state);
+    int drawntreasure=0;
+    int cardDrawn;
+    int temphand[MAX_HAND];
+    int z = 0;
+    while(drawntreasure<2){
+        if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+            shuffle(currentPlayer, state);
+        }
+        drawCard(currentPlayer, state);
+        cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+        if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+            drawntreasure++;
+        else{
+            temphand[z]=cardDrawn;
+            state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+            z++;
+        }
+    }
+
+    //discard adventurer from hand?
+
+    while(z-1>=0){
+        state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+        z=z-1;
+    }
+
+
+    return 0;
+}
+
+int playSmithy(struct gameState *state, int handPos) {
+    int currentPlayer = whoseTurn(state);
+
+    //+3 Cards
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        drawCard(currentPlayer, state);
+    }
+
+    //discard card from hand
+    discardCard(handPos, currentPlayer, state, 0);
+    return 0;
+
+}
+
+int playVillage(struct gameState *state, int handPos) {
+
+    return 0;
+}
+
+int playFeast(struct gameState *state, int choice1) {
+    return 0;
+}
+
+int playCouncil_Room(struct gameState *state, int handPos) {
+    return 0;
+}
+
 
 
 //end of dominion.c
--- projects/swansonb/dominion/refactor.c
+++ projects/swansonb/dominion/refactor.c
@@ -2,7 +2,11 @@
 
 Assignment 2
 
+playAdventurere and playSmithy are implemented as is in a seperate functions with necesary local variables defined
+other 3 are left as stubs
+refactor tested by running playdom
 
+------------------------------------------------------------------------------------
 
 */
 
