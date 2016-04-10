97c510ae25c9e0f16c320c058803cc921a156738
James Taylor <tayljame@oregonstate.edu>
2016-04-10 16:53:11 -0600

    Added files via upload

---
+++ projects/tayljame/dominion/refactor.c
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+/*************************************************************************
+ * Mark Rushmere
+ * CS 362
+ * Assignment 2
+ * Description: Descrptions of changes made to dominion.c
+ * **********************************************************************/
+
+
+// Smithy
+
+
+
+// Adventurer
+
+
+
+
+=======
+//Process of Refactoring
+/*
+	I started by looking over the dominion header and C file. I made a seperate function for adventurer
+	but named it "Adventurer". This didn't work as expected. Upon closer examination of the header file
+	I noticed the playAdventurer() prototype function. Afterwards I added that to the main source file as 
+	my function seperate from the switch statement. Then it was just a matter of looking up how to pass 
+	structs and arrays to functions before they all worked.
+	
+	The errors that I created are a pointer on the function call in playAdventurer() and declaring the iterative
+	looping integer "i" in the playSmithy function.
+*/
+>>>>>>> 10dda0265d82c7973e7a193fdc915b7b09a6fed0
