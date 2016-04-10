4b1b3c4a4afb810e76f6c2d6460f18ac8e904a71
willmichael <oleemir@gmail.com>
2016-04-09 22:56:28 -0700

    replacing documentation with my documentation

--- projects/leemi/dominion/documentation.c
+++ projects/leemi/dominion/documentation.c
@@ -1,40 +1,31 @@
-/*************************************************************************
- * Mark Rushmere
- * CS 362
+/*************************************************
+ * Michael Lee   
+ * CS362    
  * Assignment 2
- * Description: A written description of the Smithy and Adventurer cards
- * and the  discardCard() and updateCoins() function
- ************************************************************************/
+ *************************************************/
 
-// Smithy:
-// When a player plays the Smithy card, they can draw an additional 3 cards. It does not affect how many actions or coins the player has.
+//Smithy Card understanding
 
+The Smithy card allows the player to draw three extra cards and then it  calls dicardhand to discard itself.
 
 
-// Adventurer:
-// When a player plays the Adventurer cards, they can draw cards from their deck until they reveal 2 treasure cards. They place the 2 treasure
-// cards in their hand and any other cards that were drawn back in their deck.
+//Adventure Card understanding
+The adventure card draws cards until they get two treasure cards. The case loops until this condition is met and it will reshuffle the discard pile if the deck is empty. Finally the cards that aren't treasure cards are all put into the discard pile.
 
+//discardCard() understanding
+input: handpos, player, gamestate, trashflag
+output: returns nothing
+description: Removes card from player hand (takes out of play),card is trashed if trashflag is >=1, number of cards in hand is decremented, next card is put up for play
 
-// discardCard():
-// The discardCard function is passed the current player (int), hand position (int), the game state (struct gameState), and a trash flag (int).
-// 
-// The first thing the function does is check if the trash flag is set. If it is not set, then the card is added to the play pile. This is done by
-// setting the playedCards variable in the game state struct equal the current player's hand position, and then incrementing the played card count.
-// Next, the card that was played is set to -1.
-// The played card is then removed from the player's hand. If the played card was the only card in the player's hand, then the handCount (number of
-// cards of current player) is decreased by one. If the played card was the last card in the player's hand (e.g. the 5th card in a hand of 5) then the 
-// hand count of the current player's hand is also decreased by one. Otherwise, the played card is replaced with the last card in the player's hand,
-// the last card is set to equal -1, and the number of cards is decreased by one (the last card is removed).
+either the card in play goes to the trash pile or the discard pile and the player gets the next card in his hand.
 
+//updateCoins() understanding
 
-// updateCoins():
-// The updateCoins functions is passed the player (int), game state (struct gameState), and bonus (int)
-//
-// First, the function resets the coin count to zero by accessing the coins variable in the game state struct
-// Next, for each card in the player's hand, the function checks if it is a copper, silver, or gold card. If it is a copper card, the coins variable
-// in the game state struct is increased by 1, if it is a silver it is increased by 2, and if it is a gold then it is increased by 3.
-// Finally, the bonus is added to the coins varaible. 
+input: player, gamestate, bonus
+output: returns nothing (updates state)
+description: adds coins based on the treasures in the players hands; the players purchasing power
+
+when the player has a new hand he can purchase with his coins: copper, silver, gold. He can also get a bonus from the card he plays.
 
 
 
