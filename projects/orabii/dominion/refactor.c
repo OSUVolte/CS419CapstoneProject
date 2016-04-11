f0ffbe72c0712362c4736b0efcde71fdaccbe2cc
Ismail Orabi <ismail.orabi@gmail.com>
2016-04-11 00:38:48 -0400

    adding assignment 2

---
+++ projects/orabii/dominion/refactor.c
@@ -0,0 +1,5 @@
+play_adventurer: Started drawntreasure counter at 1
+play_councilroom: Started the draw card loop counter at 1
+play_smithy: Started the draw card loop counter at 1
+play_village: Enabled trash bit
+play_greathall: Not change
