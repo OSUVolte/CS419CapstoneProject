193d5ba8afbc60c0837e132704f1b76cc8661b6f
Kelby Faessler <kmfaessler@gmail.com>
2016-04-10 20:05:46 -0700

    added updated dominion.c/dominion.h, new doc files

---
+++ projects/faesslek/dominion/documentation.c
@@ -0,0 +1,23 @@
+/*
+CS 362 - Assignment 2
+Kelby Faessler
+ONID: faesslek
+
+Description of discardCard():
+    Checks a trash flag passed as argument to determine if card goes into played card pile
+    Sets value for current card to -1 (for particular hand for particular player, both passed as arguments)
+    Performs several checks on the hand count to determine how to discard 
+
+Description of updateCoins():
+    Counts treasure in players hand and doles appropriate coins
+    Adds bonus coins (bonus amount passed as argument
+
+Description of smithy card:
+    Draw 3 cards (calling the drawCard() function
+    Discard
+
+Description of adventurer card
+    Draws cards until 2 treasure cards have been drawn, adding coins to player hand
+    Discard drawn cards
+
+*/
