611dc13153263b404c6532d59ab89795266501e1
Josh Geller <josh@covalentcareers.com>
2016-04-12 08:06:36 -0400

    Add unit test 1

---
+++ projects/gellerj/dominion/unittest1.c
@@ -0,0 +1,45 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+#define VERBOSE 1
+
+int main() {
+    struct gameState G;
+    int players = 2;
+    int seed = 1;
+    int gameOver = 0;
+    int kingdomCards[10] = {
+            adventurer,
+            gardens,
+            embargo, village,
+            minion, mine,
+            cutpurse,
+            sea_hag,
+            tribute,
+            smithy
+    };
+
+    initializeGame(players, kingdomCards, seed, &G);
+
+    if (VERBOSE) printf("Test game over when province supply equals zero...\n");
+    int supplyCount = G.supplyCount[province];      // save current supply count
+    G.supplyCount[province] = 0;                    // set province to supply to 0 (game over)
+    gameOver = isGameOver(&G);
+    if (VERBOSE) printf("Game over = %d, expected 1\n", gameOver);
+    assert(gameOver == 1);
+
+    if (VERBOSE) printf("Test game over when three supply pile counts equal zero...\n");
+    G.supplyCount[province] = supplyCount;          // restore supply count for new test case
+    G.supplyCount[0] = 0;                           // set three supply pile counts to zero (game over)
+    G.supplyCount[1] = 0;
+    G.supplyCount[2] = 0;
+    gameOver = isGameOver(&G);
+    if (VERBOSE) printf("Game over = %d, expected 1\n", gameOver);
+    assert(gameOver == 1);
+
+    printf("All tests passed.\n");
+}
