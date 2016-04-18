a86e1db73347fdcf6eb1e19b7f21e73180c87c0d
Shawn Seibert <seiberts@onid.oregonstate.edu>
2016-04-17 22:04:43 -0400

    Updated unit tests, added bug1.c

--- projects/seiberts/dominion/unittest1.c
+++ projects/seiberts/dominion/unittest1.c
@@ -1,2 +1,51 @@
 //Shawn Seibert
 //Unit test 1
+//updateCoins()
+
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+int player = 1;
+struct gameState game, state;
+int bonus;
+int i;
+int seed = 100;
+int numPlayers = 2;
+int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, 
+			smithy, council_room};
+
+			initializeGame(numPlayers, k, seed, &game);
+
+printf("-----------------Unit Test 1 ----------------------");	
+printf("--------------- updateCoins() ----------------------");
+  //reset coin count
+  state->coins = 0;
+  //add coins for each Treasure card in player's hand
+  for (i = 0; i < state->handCount[player]; i++)
+    {
+      if (state->hand[player][i] == copper)
+	{
+	  state->coins += 1;
+	}
+      else if (state->hand[player][i] == silver)
+	{
+	  state->coins += 2;
+	}
+      else if (state->hand[player][i] == gold)
+	{
+	  state->coins += 3;
+	}	
+    }	
+
+  //add bonus
+  state->coins += bonus;
+
+  return 0;
+}
--- projects/seiberts/dominion/unittest2.c
+++ projects/seiberts/dominion/unittest2.c
@@ -1,2 +1,62 @@
 //Shawn Seibert
 //Unit test 2
+//gainCard()
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main()
+{
+int supplyPos;
+struct gameState *state;
+int toFlag;
+int player;
+int seed = 100;
+int numPlayers = 2;
+int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, 
+			smithy, council_room};
+
+						initializeGame(numPlayers, k, seed, &game);
+
+printf("-----------------Unit Test 2 ----------------------");	
+printf("----------------- gainCard() ----------------------");
+
+  //Note: supplyPos is enum of choosen card
+	
+  //check if supply pile is empty (0) or card is not used in game (-1)
+  if ( supplyCount(supplyPos, state) < 1 )
+    {
+      return -1;
+    }
+	
+  //added card for [whoseTurn] current player:
+  // toFlag = 0 : add to discard
+  // toFlag = 1 : add to deck
+  // toFlag = 2 : add to hand
+
+  if (toFlag == 1)
+    {
+      state->deck[ player ][ state->deckCount[player] ] = supplyPos;
+      state->deckCount[player]++;
+    }
+  else if (toFlag == 2)
+    {
+      state->hand[ player ][ state->handCount[player] ] = supplyPos;
+      state->handCount[player]++;
+    }
+  else
+    {
+      state->discard[player][ state->discardCount[player] ] = supplyPos;
+      state->discardCount[player]++;
+    }
+	
+  //decrease number in supply pile
+  state->supplyCount[supplyPos]--;
+	 
+  return 0;
+}
