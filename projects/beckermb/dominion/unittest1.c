cdec0ca4dfc3a158196e9271b706f93ee4f5fcc1
Ben Beckerman <beckermb@oregonstate.edu>
2016-04-18 19:31:14 -0700

    finish first unit test

--- projects/beckermb/dominion/unittest1.c
+++ projects/beckermb/dominion/unittest1.c
@@ -14,7 +14,20 @@ int main () {
   int cards[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
   initializeGame(4, cards, 1, &game);
 
-  //Brand new game. Expect game to not be over.
-  assert(isGameOver(game) == 0)
+  //Brand new game. Expect game to be in progress.
+  assert(isGameOver(&game) == 0);
 
+  // Zero provinces. Expect game to be over.
+  game.supplyCount[province] = 0;
+  assert(isGameOver(&game) == 1);
+  game.supplyCount[province] = 5;
+
+  //Two stacks empty. Expect game to be in progress.
+  game.supplyCount[council_room] = 0;
+  game.supplyCount[gardens] = 0;
+  assert(isGameOver(&game) == 0);
+
+  //Three stacks empty. Expect game to be over.
+  game.supplyCount[smithy] = 0;
+  assert(isGameOver(&game) == 1);
 }
