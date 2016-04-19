ac7761ac96cb96efa7b02400ee23190eb6ba5a2e
Ben Beckerman <beckermb@oregonstate.edu>
2016-04-18 19:14:37 -0700

    fixed test init

--- projects/beckermb/dominion/unittest1.c
+++ projects/beckermb/dominion/unittest1.c
@@ -3,20 +3,18 @@
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-#include "rngs.h"
 
 
 //This is a test of the isGameOver method in dominion.c
 
 int main () {
-    printf("Testing isGameOver\n");
+  printf("Testing isGameOver\n");
 
-    struct gameState game;
-    int i; //counter for initialization
-      for (i = 0; i < 25; i++) {
-      game->supplyCount[i] = 10;
-    }
-    //All piles full. Expect game to be not over.
-    assert(isGameOver(game) == 0)
+  struct gameState game;
+  int cards[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
+  initializeGame(4, cards, 1, &game);
+
+  //Brand new game. Expect game to not be over.
+  assert(isGameOver(game) == 0)
 
 }
