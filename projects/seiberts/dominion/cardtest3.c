0bbef60d7ca5f16c23ec73dbaab8cd361bb37a35
Shawn Seibert <seiberts@onid.oregonstate.edu>
2016-04-18 21:50:52 -0400

    updated 4 cardtest files

--- projects/seiberts/dominion/cardtest3.c
+++ projects/seiberts/dominion/cardtest3.c
@@ -1,2 +1,30 @@
 //Shawn Seibert
 //Card Test 3
+//councilRoomCard()
+
+
+int councilRoomCard(int handPos, int currentPlayer, struct gameState *state)
+{
+	int i = 0;
+	     //+4 Cards
+      for (i = 0; i <= 4; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+			
+      //+1 Buy
+      state->numActions++;
+			
+      //Each other player draws a card
+      for (i = 0; i < state->numPlayers; i++)
+	{
+	  if ( i != currentPlayer )
+	    {
+	      drawCard(i, state);
+	    }
+	}
+			
+      //put played card in played card pile
+      discardCard(handPos, currentPlayer, state, 0);
+	  return 0;
+}
