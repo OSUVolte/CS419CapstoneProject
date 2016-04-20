855b8a27c02dd9d7b844649edb6614ee9b3ceab4
rostun <rosa.m.tung@gmail.com>
2016-04-19 21:24:30 -0400

    project skeleton

---
+++ projects/tungr/dominion/unittest1.c
@@ -0,0 +1,20 @@
+/*
+	Rosa Tung
+	CS362
+	(20 points) Write unit tests for four functions (not card implementations or cardEffect) in dominion.c. 
+	Check these tests in as unittest1.c,unittest2.c, unittest3.c, and unittest4.c. 
+	(for example, you can create a test for updateCoins() method)
+*/
+
+#include "dominion.h" 
+#include "dominion_helpers.h" 
+
+#include <string.h> 
+#include <stdio.h> //printf scanf
+#include <assert.h> 
+#include <stdlib.h> //free malloc
+
+int main(){
+	printf("unittest1.c\n");
+	return 0;
+}
