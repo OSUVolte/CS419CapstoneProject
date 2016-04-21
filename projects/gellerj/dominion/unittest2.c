71cd701b4eeab8dd49ccf7917e2f6d564568ff9c
Josh Geller <josh@covalentcareers.com>
2016-04-20 20:36:52 -0400

    Update unit test 2

--- projects/gellerj/dominion/unittest2.c
+++ projects/gellerj/dominion/unittest2.c
@@ -5,82 +5,104 @@
 #include <assert.h>
 #include "rngs.h"
 
-#define VERBOSE 1
-
 int main() {
-    struct gameState G;
-    int players = 2;
+    struct gameState G1, G2;
+    int numPlayers = 2;
     int seed = 1;
-    int gameOver = 0;
-    int result = 0;
+    int player1 = 0;
+    int player2 = 1;
+    int bonus = 0;
+    int handPos = 0;
+    int c1, c2, c3 = 0;
+    int result = -1;
     int kingdomCards[10] = {
             adventurer,
             gardens,
-            embargo, village,
-            minion, mine,
+            embargo,
+            village,
+            minion,
+            mine,
             cutpurse,
             sea_hag,
             tribute,
             smithy
     };
 
-    initializeGame(players, kingdomCards, seed, &G);
+    initializeGame(numPlayers, kingdomCards, seed, &G1);
+    memcpy(&G2, &G1, sizeof(struct gameState));
 
     printf("Testing gainCard():\n");
 
-    if (VERBOSE)
-        printf("\tTest card is not gained if supply count equals zero...\n");
-    G.supplyCount[smithy] = 0;                       // set the supply count for this card to 0
-    result = gainCard(smithy, &G, 1, 1);
-    if (VERBOSE)
-        printf("\t\tReturn value = %d, expected -1\n", result);
-    assert(result == -1);
+    printf("\nTest card is not gained if supply count equals zero...\n");
+    G2.supplyCount[smithy] = 0;
+    result = gainCard(smithy, &G2, 1, player1);
+    printf("Return value = %d, expected -1...", result);
+    if (result == -1) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
 
-    initializeGame(players, kingdomCards, seed, &G);
+    memcpy(&G2, &G1, sizeof(struct gameState));
 
-    if (VERBOSE)
-        printf("\tTest card gained is added to deck...\n");
-    gainCard(smithy, &G, 1, 1);
-    int lastDeckCard = G.deck[1][G.deckCount[1] - 1];
-    if (VERBOSE)
-        printf("\t\tLast card in deck = %d, expected 13 (smithy)\n", lastDeckCard);
-    assert(lastDeckCard == smithy);
-    if (VERBOSE)
-        printf("\t\tP1 deck count = %d, expected 11\n", G.deckCount[1]);
-    assert(G.deckCount[1] == 11);
+    printf("\nTest card gained is added to deck...\n");
+    gainCard(smithy, &G2, 1, player1);
+    int lastDeckCard = G2.deck[player1][G2.deckCount[player1] - 1];
+    printf("Last card in deck = %d, expected 13 (smithy)...", lastDeckCard);
+    if (lastDeckCard == smithy) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
+    printf("Player 1 deck count = %d, expected %d...", G2.deckCount[player1], G1.deckCount[player1] + 1);
+    if (G2.deckCount[player1] == G1.deckCount[player1] + 1) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
 
-    initializeGame(players, kingdomCards, seed, &G);
+    memcpy(&G2, &G1, sizeof(struct gameState));
 
-    if (VERBOSE)
-        printf("\tTest card gained is added to hand...\n");
-    gainCard(smithy, &G, 2, 1);
-    int lastHandCard = G.hand[1][G.handCount[1] - 1];
-    if (VERBOSE)
-        printf("\t\tLast card in hand = %d, expected 13 (smithy)\n", lastHandCard);
-    assert(lastHandCard == smithy);
-    if (VERBOSE)
-        printf("\t\tP1 hand count = %d, expected 1\n", G.handCount[1]);
-    assert(G.handCount[1] == 1);
+    printf("\nTest card gained is added to hand...\n");
+    gainCard(smithy, &G2, 2, player1);
+    int lastHandCard = G2.hand[player1][G2.handCount[player1] - 1];
+    printf("Last card in hand = %d, expected 13 (smithy)...", lastHandCard);
+    if (lastHandCard == smithy) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
+    printf("P1 hand count = %d, expected %d...", G2.handCount[player1], G1.handCount[player1] + 1);
+    if (G2.handCount[player1] == G1.handCount[player1] + 1) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
 
-    initializeGame(players, kingdomCards, seed, &G);
+    memcpy(&G2, &G1, sizeof(struct gameState));
 
-    if (VERBOSE)
-        printf("\tTest card gained is added to discard...\n");
-    int supplyCount = G.supplyCount[smithy];
-    gainCard(smithy, &G, 0, 1);
-    int lastDiscardCard = G.discard[1][G.discardCount[1] - 1];
-    if (VERBOSE)
-        printf("\t\tLast card in hand = %d, expected 0\n", lastDiscardCard);
-    assert(lastDiscardCard == smithy);
-    if (VERBOSE)
-        printf("\t\tP1 discard count = %d, expected 1\n", G.discardCount[1]);
-    assert(G.discardCount[1] == 1);
+    printf("\nTest card gained is added to discard...\n");
+    gainCard(smithy, &G2, 0, player1);
+    int lastDiscardCard = G2.discard[player1][G2.discardCount[player1] - 1];
+    printf("Last card in hand = %d, expected %d...", lastDiscardCard, smithy);
+    if (lastDiscardCard == smithy) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
+    printf("Player 1 discard count = %d, expected 1...", G2.discardCount[player1]);
+    if (G2.discardCount[player1] == 1) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
 
-    if (VERBOSE)
-        printf("\tTest supply pile count is decremented...\n");
-    if (VERBOSE)
-        printf("\t\tSupply count = %d, expected %d\n", G.supplyCount[0], supplyCount - 1);
-    assert(G.supplyCount[smithy] == supplyCount - 1);
+    printf("\nTest supply pile count is decremented...\n");
+    printf("Supply count = %d, expected %d...", G2.supplyCount[smithy], G1.supplyCount[smithy] - 1);
+    if (G2.supplyCount[smithy] == G1.supplyCount[smithy] - 1) {
+        printf("PASSED.\n");
+    } else {
+        printf("FAILED.\n");
+    }
 
-    printf("All tests passed.\n");
 }
