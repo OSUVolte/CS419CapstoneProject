a70f9dfffcc24e63cc7f4945c27658f5d389bfb3
Ben Beckerman <beckermb@oregonstate.edu>
2016-04-20 19:26:00 -0700

    added second card test

--- projects/beckermb/dominion/dominion.h
+++ projects/beckermb/dominion/dominion.h
@@ -137,6 +137,6 @@ int playFeast(struct gameState *state, int choice1);
 int playCouncil_Room(struct gameState *state, int handPos);
 
 int smithyCardEffect(int currentPlayer, int handPos, struct gameState *state);
-
+int outpostCardEffect(int currentPlayer, int handPos, struct gameState *state);
 
 #endif
