85ca9a473600beb041ce03a771c877c16e6c8c72
cuellarj <j.cue@hotmail.com>
2016-04-20 18:26:44 -0700

    Assignment 3

--- projects/cuellarj/dominion/refactor.c
+++ projects/cuellarj/dominion/refactor.c
@@ -1,6 +1,6 @@
 Changed cards
 callAdventurerCard()  -   cardDrawn condition is wrong & returns the wrong value
-callSmithyCard()      -   for loop is made infinite - jams program 
+callSmithyCard()      -   for loop gives 6 cards instead of 3
 callVillageCard()     -   drawCard() is not called
 callEmbargoCard()     -   Embargo does not add the tokens correctly 
 callOutpostCard()     -   NO CHANGES - outpost card doesn't have any changes, it's too simple
