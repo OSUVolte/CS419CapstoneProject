a2e128d359a6bd6dd5fe6d01d54a08150dec3a6b
chrijohn <chrijohn@onid.oregonstate.edu>
2016-04-21 20:59:31 -0500

    unittest1 and unitest2 added

--- projects/chrijohn/dominion/unittest1.c
+++ projects/chrijohn/dominion/unittest1.c
@@ -5,9 +5,6 @@
 #include <assert.h>
 #include "rngs.h"
 
-// set NOISY_TEST to 0 to remove printfs from output
-#define NOISY_TEST 1
-
 int main() {
 	int i;
 	int seed = 1000;
@@ -23,11 +20,13 @@ int main() {
 	int coppers[MAX_HAND];
 	int silvers[MAX_HAND];
 	int golds[MAX_HAND];
+	int noCoins[MAX_HAND];
 	for (i = 0; i < MAX_HAND; i++)
 	{
 		coppers[i] = copper;
 		silvers[i] = silver;
 		golds[i] = gold;
+		noCoins[i] = gardens;
 	}
 
 	printf("TESTING updateCoins():\n");
@@ -37,32 +36,24 @@ int main() {
 		{
 			for (bonus = 0; bonus <= maxBonus; bonus++)
 			{
-#if (NOISY_TEST == 1)
-				printf("Test player %d with %d treasure card(s) and %d bonus.\n", p, handCount, bonus);
-#endif
 				memset(&G, 23, sizeof(struct gameState));   // clear the game state
 				r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
 				G.handCount[p] = handCount;                 // set the number of cards on hand
 				memcpy(G.hand[p], coppers, sizeof(int) * handCount); // set all the cards to copper
 				updateCoins(p, &G, bonus);
-#if (NOISY_TEST == 1)
-				printf("G.coins = %d, expected = %d\n", G.coins, handCount * 1 + bonus);
-#endif
 				assert(G.coins == handCount * 1 + bonus); // check if the number of coins is correct
 
 				memcpy(G.hand[p], silvers, sizeof(int) * handCount); // set all the cards to silver
 				updateCoins(p, &G, bonus);
-#if (NOISY_TEST == 1)
-				printf("G.coins = %d, expected = %d\n", G.coins, handCount * 2 + bonus);
-#endif
 				assert(G.coins == handCount * 2 + bonus); // check if the number of coins is correct
 
 				memcpy(G.hand[p], golds, sizeof(int) * handCount); // set all the cards to gold
 				updateCoins(p, &G, bonus);
-#if (NOISY_TEST == 1)
-				printf("G.coins = %d, expected = %d\n", G.coins, handCount * 3 + bonus);
-#endif
 				assert(G.coins == handCount * 3 + bonus); // check if the number of coins is correct
+
+				memcpy(G.hand[p], noCoins, sizeof(int) * handCount); // set all the cards to gold
+				updateCoins(p, &G, bonus);
+				assert(G.coins == handCount * 0 + bonus); // check if the number of coins is correct
 			}
 		}
 	}
