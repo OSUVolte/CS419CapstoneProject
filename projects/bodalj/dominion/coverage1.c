acca5071b874fcdc1cee93ca552d1d5fe52a0ef5
Jens Bodal <bodalj@onid.oregonstate.edu>
2016-04-22 20:31:30 -0700

    Added first unit test

---
+++ projects/bodalj/dominion/coverage1.c
@@ -0,0 +1,15 @@
+**Use gcov to measure code coverage for all of these tests. Report findings by discussing tests' coverage (statement, branch, boundary, etc.) and describe their 
+impliciations for tests here.  **
+
+
+## Unit Test 1: initializeGame function
+
+    initializeGame(int numPlayers, int kingdomCards[10], int randomSeed, struct gameState *state);
+
+* number of players needs to be between 2 - max players
+* kingdom cards must be different
+* must be 10 kingdom cards total (not a requirement?) it's a game requirement, so I should add it
+* number of curse cards 
+
+
+
