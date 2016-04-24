9d8cf29eef15d04a150457e9cb1e3a7a79adabbf
firenebula <brownjoh@onid.oregonstate.edu>
2016-04-23 23:16:59 -0500

    finished with assignment 3

--- projects/brownjoh/dominion/cardtest3.c
+++ projects/brownjoh/dominion/cardtest3.c
@@ -5,12 +5,13 @@
 #include "rngs.h"
 #include <stdlib.h>
 
-//testing gainCard function
+//testing playVillage(struct gameState *state, int handPos, int currentPlayer)
 
 int testVals(int expected, int actual, char *testName);
-int runTest(int supplyPos, struct gameState *state, int toFlag, int player);
+int runTest(struct gameState *state, int handPos, int currentPlayer);
 
 int main() {
+
 
 //initializing game
 int numPlayers = 2;
@@ -18,31 +19,35 @@ int seed = 555;
 struct gameState Gstate, testGState;
 int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
 initializeGame(numPlayers, k, seed, &Gstate);
+int i = 0;
 
 
 
-//precondition, the card type, game state, position the card is going to hand (discard deck or hand)  and player number passed in.
-// post condition, the chosen card should be added to the requested position for the selected player
+//precondition, game state, card position and current player is passed to the function
+// post condition, one additional card is added to the hand, two additional actions added
 
 //int gainCard(int supplyPos, struct gameState *state, int toFlag, int player)
+
+	printf("\n***Begin Testing playVillage***\n");
+
 	//copying gameState
 	memcpy(&testGState, &Gstate, sizeof(struct gameState));
-	runTest(1 , &testGState, 0, whoseTurn(&testGState));
-	
-	memcpy(&testGState, &Gstate, sizeof(struct gameState));
-	runTest(6 , &testGState, 5, whoseTurn(&testGState));
+	gainCard(village, &testGState, 2, whoseTurn(&testGState));
+	runTest(&testGState, 0, whoseTurn(&testGState));
 
 	memcpy(&testGState, &Gstate, sizeof(struct gameState));
-	runTest(8 , &testGState, 1, whoseTurn(&testGState));
-	
-	memcpy(&testGState, &Gstate, sizeof(struct gameState));
-	runTest(7 , &testGState, 2, whoseTurn(&testGState));
+	gainCard(tribute, &testGState, 2, whoseTurn(&testGState));
+	gainCard(village, &testGState, 2, whoseTurn(&testGState));
+	runTest(&testGState, 1, whoseTurn(&testGState));
 
 	memcpy(&testGState, &Gstate, sizeof(struct gameState));
-	runTest(1 , &testGState, 3, whoseTurn(&testGState));
+	for(i = 0; i < 5; i++)
+		gainCard(smithy, &testGState, 2, whoseTurn(&testGState));
+	for(i = 0; i < 3; i++)
+	gainCard(village, &testGState, 2, whoseTurn(&testGState));
+	runTest(&testGState, 06, whoseTurn(&testGState));
 	
-	memcpy(&testGState, &Gstate, sizeof(struct gameState));
-	runTest(5 , &testGState, 0, whoseTurn(&testGState));
+
 
 
 	return 0;
@@ -62,48 +67,35 @@ int testVals(int expected, int actual, char *testName){
 	else
 		printf("Failed!\n");
 
+	return 0;
+
 
 }
 
-int runTest(int supplyPos, struct gameState *state, int toFlag, int player){
-
+int runTest(struct gameState *state, int handPos, int currentPlayer){
+		//precondition, game state, card position and current player is passed to the function
+		// post condition, one additional card is added to the hand, two additional actions added
 
 	// toFlag = 0 : add to discard
   // toFlag = 1 : add to deck
   // toFlag = 2 : add to hand
-	switch(toFlag){
-			int expectedCount = -1;
+	int startingHand;
+	int endingHand;
+	int startingAction;
+	int endingAction;
 
-		case 0 :
-		expectedCount = (state->discardCount[player]) + 1;
-		gainCard(supplyPos, state, toFlag, player);
-		testVals(expectedCount, state->discardCount[player], "discard size");
-		break;
-
-		case 1 :
-		expectedCount = (state->deckCount[player]) + 1;
-		gainCard(supplyPos, state, toFlag, player);
-		testVals(expectedCount, state->deckCount[player], "deck size");
-
-		break;
-
-		case 2 :
-		expectedCount = (state->handCount[player]) + 1;
-		gainCard(supplyPos, state, toFlag, player);
-		testVals(expectedCount, state->handCount[player], "hand size");
-
-		break;
-
-
-		default : //should be no change
-		expectedCount = (state->handCount[player]);
-		gainCard(supplyPos, state, toFlag, player);
-		testVals(expectedCount, state->handCount[player], "hand size");
-
-	}
+	startingAction = state->numActions;
+	startingHand = state->handCount[currentPlayer];
+	playVillage(state, handPos, currentPlayer);
+	endingAction = state->numActions;
+	endingHand = state->handCount[currentPlayer];
+	testVals(startingAction + 2, endingAction, "Number of Actions");
+	testVals(startingHand + 1, endingHand, "Number in Hand");
 
 
 	//testVals(1, 2, "badstuff");
 	//testVals(1, 1, "goodstuff");
+
+	return 0;
 
 }
