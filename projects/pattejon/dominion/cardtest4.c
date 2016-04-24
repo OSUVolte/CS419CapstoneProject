b74226a3b35a9f6af7b7e8dadfe91e6cf3e4cf53
Jon Patterson <pattejon@oregonstate.edu>
2016-04-24 09:27:35 -0600

    committing the rest of these while I work on the writeup

--- projects/pattejon/dominion/cardtest4.c
+++ projects/pattejon/dominion/cardtest4.c
@@ -1,8 +1,8 @@
 /* ---------------------------------------------------------------------
 * Jon Patterson
 * Assignment 3
-* cardtest3.c
-* council room card tests
+* cardtest4.c
+* village card tests
 *---------------------------------------------------------------------*/
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -34,38 +34,29 @@ int main() {
     for(j = 0; j < 25; j++){
         G.supplyCount[j] = 2;
     }
-    // store values of other players hands to test afterward
-    int *player_card = calloc(numPlayer, sizeof(int));
 
-    for (p = 0; p < numPlayer; p++){
-        player_card[i] = G.handCount[p];
-    }
-
-    
-    printf ("Testing council_room for playCard(0, 0, 0, 0, gamestate):\n");
+    printf ("Testing village for playCard(0, 0, 0, 0, gamestate):\n");
     p = 0;
-	printf("Testing for player %d:\n", p);
+    printf("Testing for player %d:\n", p);
     int testActions = G.numActions;
     int testBuys = G.numBuys;
     int testHandCount = G.handCount[p];
     int testDeckCount = G.deckCount[p];
 
     // make card in hand a council_room
-    G.hand[p][0] = council_room;
+    G.hand[p][0] = village;
     //play it
     playCard(0, 0, 0, 0, &G);
 
- 	//Upon execution, adjust test values
- 	//test actions should be one less
- 	testActions--;
-    //Buys are incremented by 1
-    testBuys++;
- 	//net hand count should now be +3
- 	testHandCount += 3;
- 	//deck count should be 3 less
- 	testDeckCount -= 4;
+    //Upon execution, adjust test values
+    //test actions should be one more
+    testActions++;
+    //Buys unchanged
+    //net hand count should be unchanged
+    //deck count should be 1 less
+    testDeckCount -= 1;
 
- 	#if (NOISY_TEST == 1)
+    #if (NOISY_TEST == 1)
     printf("Test actions updated correctly:\n");
     printf("Actions = %d, Expected = %d\n", G.numActions, testActions);
     printf("Test buys updated correctly:\n");
@@ -83,26 +74,13 @@ int main() {
     #endif
     if (G.numActions != testActions) error = 1;
     if (G.numBuys != testBuys) error = 1;
-    if (G.deckCount[p] != testDeckCount	) error = 1;
-    if (G.handCount[p] != testHandCount	) error = 1;
+    if (G.deckCount[p] != testDeckCount ) error = 1;
+    if (G.handCount[p] != testHandCount ) error = 1;
 
-    //test other players hands to ensure that they were incremented by one
-    for(p = 1; p < numPlayer; p++){
-        #if (NOISY_TEST == 1)
-        printf("Checking the hands of other players:\n");
-        printf("Player %d Hand = %d, Expected = %d\n", p, G.handCount[p], player_card[i]+1);
-        #endif
-        #if (ASSERTS_ON == 1)
-        assert(G.handCount[p] == player_card[i]+1);
-        #endif
-        if(G.handCount[p] != player_card[i]+1) error = 1;
-    }
-    free(player_card);
     if(error == 1){
-    	printf("Errors were encountered.\n");
-    	return 1;
+        printf("Errors were encountered.\n");
+        return 1;
     }
-
 
     return 0;
 }
