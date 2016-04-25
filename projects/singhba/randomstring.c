0a0040d7c912445b3ae75b7515580c16d80c01af
Baljot Singh <baljotsingh1986@yahoo.com>
2016-04-24 22:55:39 -0700

    Update randomstring.c

--- projects/singhba/randomstring.c
+++ projects/singhba/randomstring.c
@@ -3,6 +3,6 @@ To get error message; input string needs to return word 'reset'
 For this I implemented inputChar() to return a char randomly
 and inside inputString() I used inputChar() to create string
 and that string is returned to testme(). Once testme() gets 
-'reset' from inputString() , it reaches state 9 and prints
+'reset' from inputString() , it prints
 error message and exit the program. 
 */
