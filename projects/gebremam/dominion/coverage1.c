5b208c18696311db3dac6406660e408a9e631c49
marthaGM <gebremam@oregonstate.edu>
2016-04-25 21:36:15 -0600

    coverage1.c

---
+++ projects/gebremam/dominion/coverage1.c
@@ -0,0 +1,97 @@
+/* -----------------------------------------------------------------------
+ * Martha Gebremariam
+ * CS362_SP2016
+ * Assignment 3
+ * Description: coverage1.c describing code coverage for unit test.
+ * -----------------------------------------------------------------------
+ */
+/*
+TEST COVERAGE FOR DOMINION
+
+File 'dominion.c'
+Lines executed:42.45% of 556
+
+Running 'gcov -b dominion' results:-
+
+Lines executed:42.45% of 556
+Branches executed:45.32% of 417
+Taken at least once:35.25% of 417
+Calls executed:30.53% of 95
+Creating 'dominion.c.gcov'
+
+This shows that 42.45% of 556 (~236) lines were covered with the tests.
+This is mainly because the tests were only covering 8 functions within dominion.c.
+
+
+Running 'gcov -b unittest1' results:-
+File 'unittest1.c'
+Lines executed:95.56% of 45
+Branches executed:100.00% of 20
+Taken at least once:95.00% of 20
+Calls executed:90.00% of 10
+Creating 'unittest1.c.gcov'
+
+Running 'gcov -b unittest2' results:-
+
+File 'unittest2.c'
+Lines executed:82.91% of 117
+Branches executed:100.00% of 24
+Taken at least once:58.33% of 24
+Calls executed:86.49% of 74
+Creating 'unittest2.c.gcov'
+
+Running 'gcov -b unittest3' results:-
+
+File 'unittest3.c'
+Lines executed:72.55% of 204
+Branches executed:100.00% of 56
+Taken at least once:50.00% of 56
+Calls executed:83.72% of 172
+Creating 'unittest3.c.gcov'
+
+Running 'gcov -b unittest4' results:-
+
+File 'unittest4.c'
+Lines executed:73.53% of 408
+Branches executed:100.00% of 112
+Taken at least once:50.89% of 112
+Calls executed:83.38% of 331
+Creating 'unittest4.c.gcov'
+
+Running 'gcov -b cardtest1' results:-
+File 'cardtest1.c'
+Lines executed:91.67% of 24
+Branches executed:100.00% of 2
+Taken at least once:50.00% of 2
+Calls executed:88.89% of 9
+Creating 'cardtest1.c.gcov'
+
+Running 'gcov -b cardtest2' results:-
+File 'cardtest2.c'
+Lines executed:91.67% of 24
+Branches executed:100.00% of 2
+Taken at least once:50.00% of 2
+Calls executed:88.89% of 9
+Creating 'cardtest2.c.gcov'
+
+Running 'gcov -b cardtest3' results:-
+
+File 'cardtest3.c'
+Lines executed:86.67% of 30
+Branches executed:100.00% of 4
+Taken at least once:50.00% of 4
+Calls executed:84.62% of 13
+Creating 'cardtest3.c.gcov'
+
+Running 'gcov -b cardtest4' results:-
+File 'cardtest4.c'
+Lines executed:87.50% of 32
+Branches executed:100.00% of 4
+Taken at least once:50.00% of 4
+Calls executed:85.71% of 14
+Creating 'cardtest4.c.gcov'
+
+
+
+*/
+
