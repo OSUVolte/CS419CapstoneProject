bed4d6ef9d0a429812fb6b4a30971256cb12522a
chrisjohns <chrisjohns210@gmail.com>
2016-04-26 00:43:19 -0700

    assignment3

---
+++ projects/johnchri/dominion/coverage1.c
@@ -0,0 +1,10 @@
+/*
+The general coverage for the unit and card tests was pretty good. The lowest coverage I got was for the init tests at
+79%. I thought I might not get everything since the init was one of the larger functions to cover. I tried to pick the
+functions to test that made the most sense to test, such as ones that had more logic and were depending on more varying options.
+My code coverage didn't make it to 100% on any of the tests but every test had 100% branch coverage. I think I have to
+be more careful making sure I cover everything. I think the tests however are fairly good. Code coverage is not a
+perfectly good indicator of how well a function is tested.
+
+
+*/
