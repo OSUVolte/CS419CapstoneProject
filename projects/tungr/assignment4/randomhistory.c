5d97e711b98b3fb01882009bb74d5556c9cc68ec
rostun <rosa.m.tung@gmail.com>
2016-04-28 13:50:56 -0400

    assignment 4 files

---
+++ projects/tungr/assignment4/randomhistory.c
@@ -0,0 +1,7 @@
+/*
+	Rosa Tung
+	CS 362 Assignment 4
+	randomhistory.c
+		Write up and Check in the development of your rnaomd testers, including improvements in coverage
+		and effort to check the correctness of your specification. Be detailed and thorough
+*/
---
+++ projects/tungr/dominion/randomtestcard.c
@@ -0,0 +1,6 @@
+/*
+	Rosa Tung
+	CS 362 Assignment 4
+	randomtestadventurer.c
+		Write a random tester for two Dominion cards, one of which has to be the adventurer card.
+*/
