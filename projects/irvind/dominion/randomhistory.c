6e1c81b42bd345345c232b1541d3ce35eb2c9a13
drummingrocks <drummingrocks@hotmail.com>
2016-05-07 02:55:18 -0400

    Adding Assignment 4

--- projects/irvind/dominion/randomhistory.c
+++ projects/irvind/dominion/randomhistory.c
@@ -17,32 +17,66 @@ looping through each of the game state variables individually and printing the r
 random test was really going to be random.  After sorting all of that out, I went through the variables once again after they were 
 passed into the game state, to make sure they hadn't changed or been reassigned.  While I had the variables printing to the screen,
 I made sure that none of them were going outside of the acceptable ranges.
-	Once the randomization and game states were set up properly, a lot of the actual tests were based on what I had from Assignment 3.
+	Once the randomization and game states were set up properly, a lot of the actual tests built on what I had from Assignment 3.
 When I ran the tester it was clear very quickly that I needed to cut down on the amount of information I was displaying to the user.  
 I changed the earlier code from displaying both 'Success' and 'Failure' messages to displaying only failures.  That made the information
 much easier to read and understand. The tester I ended up with loops for a set number of tests, creating a new/randomized game state for 
-each run.  While running, the tester tests each game state *****
-
-----Coverage----
-
-
+each run.  Each random game state is then tested for correctness, with tests occurring for the players' discard, deck, and hand count, as
+well as the card's effect on the victory and kingdom cards.
+	One thing I struggled with on both random testers was figuring out how many tests to run.  I wanted my tests to be effective, but
+I was also worried that the testers would take too long to execute, which might be interpreted as malfunctioning code.  I tried to split the
+difference: while 10,000 tests would take forever to execute, only running 5 tests would be trivial, and wouldn't be effective.  I finally
+settled on running 50 tests, which seemed like a fair compromise.  If, during testing/grading, you end up staring at a blank screen, the
+testers are still running, even though they don't seem to be.
+ 
 /*---------------------------------------------------------*/
 
 randomcardtestadventurer.c:
 
 ----Development----
-	I remembered the Adventurer card from Assignment 3; it was probably the most difficult card to test properly.  Therefore,
-I wasn't crazy about having to make a random tester for Adventurer, but it was part of the assignment.  Again, part of the 
-difficulty was in making sure that the game state was truly randomized.  Compared to the Smithy card (which I did first), however,
-the Adventurer card has quite a bit more going on.  The Smithy card basically just adds three cards to a user's deck; the Adventurerer
-card has a lot more to evaluate and test.  The basic tests were fairly easy to implement, since they were basically checking the hand count,
-deck count, discard count, and game state for errors.  
-	Checking the treasure cards, however, took some time to figure out.  
-	I implement a basic error counter that would display how many times a bug had occurred; I figured this might be a good
+	I remembered the Adventurer card from Assignment 3; it was probably the most difficult card to test properly.  Like before, part
+of the difficulty in designing a random tester was in making sure that the game state was truly randomized.  Compared to the Smithy 
+card (which I did first), the Adventurer card has quite a bit more going on.  The Smithy card basically just adds three cards to a user's 
+deck; the Adventurerer card has a lot more to evaluate and test.  The basic tests were fairly easy to implement, since they were basically
+checking the hand count, deck count, discard count, and game state for errors. 
+	The random tester for the Adventurer card is similar to the tester for the Smithy card: it loops for a pre-defined number of tests, and
+during each iteration it creates a randomized game state for each run.  Each random game state is then tested for correctness.  There are tests
+in place for the players' discard, deck, and hand counts, as well as for the presence of Treasure cards.
+	I initially implemented a basic error counter that would display how many times a bug had occurred; I figured this might be a good
 way to determine whether an error condition was a one-time fluke, or was a legitimate error.  Anything that I saw repeated, I felt fairly
 sure could be labeled as an error.  It would also help me look for patterns.  I had this designed, then realized I could just look back through
 the test results and achieve the same effect.
-  One pattern that showed up from time to time but not consistently was an error that the second card added to Player 1's 
-  
+
+/*---------------------------------------------------------*/  
   
  ----Coverage---- 
+	When evaluating the coverage of my random testers, I went back and compared the coverage rates to what I had received from the unit tests
+in Assignment 3.  Compared to the earlier assignment, my random test coverage rates actually decreased slightly.  I expected this, both because my
+random testers targeted specific bugs in the game state, and also because Assignment 3 had so many more tests.
+	It's clear that there's much more that could be done with these random testers.  My impression from these results is that both random and unit
+testing have their own advantages and disadvantages.  Neither approach is perfect, but both are useful depending on the context and test requirements.
+If I were working in a production environment, it would be critical to perform both styles of testing.  As my results below indicate, even when you use
+both types of testing, it's still very easy to overlook certain aspects or parts of your code.  In my case, I know that my random testers are limited by 
+only testing the variables of the game state.  My tests are somewhat basic, and they assume a lot of things are working correctly.  There could very well
+be a bug in the some of the more complex functions like shuffle and gainCard, and my current tests would miss them entirely.  Ideally, this is where overlap
+between random testing and unit testing would be beneficial, as you're seldom going to end up with a decent test suite if only one style of testing is used.
+
+
+--Assignment 3's results:
+For all unit tests:
+File 'dominion.c'
+Lines executed:36.11% of 565
+Branches executed:32.22% of 419
+Taken at least once:24.11% of 419
+Calls executed:27.66% of 94
+Creating 'dominion.c.gcov'
+
+
+--Assignment 4's results:
+For randomtestcard.c:								For randomtestadventurer.c:
+File 'dominion.c'									File 'dominion.c'
+Lines executed:26.20% of 561						Lines executed:23.35% of 561
+Branches executed:27.85% of 413						Branches executed:26.39% of 413
+Taken at least once:17.43% of 413					Taken at least once:15.74% of 413
+Calls executed:17.02% of 94							Calls executed:15.96% of 94
+
--- projects/irvind/dominion/randomtestcard.c
+++ projects/irvind/dominion/randomtestcard.c
@@ -2,8 +2,8 @@
 * Brett Irvin
 * 4/24/16
 * CS362_400 Software Engineering II
-* Assignment 4--randomtestadventurer.c
-* Random tester for the Adventurer card
+* Assignment 4--randomtestcard.c
+* Random tester for the Smithy card
 *---------------------------------------*/
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -14,7 +14,7 @@
 #include "rngs.h"
 #include "math.h"
 
-#define NUM_TESTS 1000
+#define NUM_TESTS 50
 
 /*Smithy Card's requirements:
 1. Current player should receive exactly 3 cards.
@@ -29,36 +29,43 @@ int main() {
 	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
 		sea_hag, tribute, smithy, council_room};
 	int thisPlayer = 0;
-	int secPlayer = 1;
 	int numPlayers = 0;
 	struct gameState G, testG;
 	SelectStream(2);
 	PutSeed(3);
 	
-    // Randomize game variables
+    // Randomize game variables--always need at least 2 players, but can have 4 maximum
 	for(i = 0; i < NUM_TESTS; i++){
-		numPlayers = floor(Random() * (MAX_PLAYERS - 2)) + 2; 
+		numPlayers = floor(Random() * (MAX_PLAYERS - 2)) + 2;
+		// printf("Num players: %d\n", numPlayers);		
 		seed = floor(Random() * 2000);
+		//printf("Seed: %d \n", seed);
+		
 		initializeGame(numPlayers, k, seed, &G);	
-		thisPlayer = floor(Random() * numPlayers);	
+		thisPlayer = floor(Random() * numPlayers);
+		//printf("thisPlayer: %d \n", thisPlayer);
 		G.whoseTurn = thisPlayer;	
+		//printf("thisPlayer NOW: %d \n", thisPlayer);
 		
 		// Create a random hand 
 		for( i = 0; i < numPlayers; i++){
-			handCount = floor(Random() * MAX_HAND); 
-			G.handCount[i] = handCount;			
-			for(j = 0; j < handCount; j++){ 
+			handCount = floor(Random() * MAX_HAND); 	// random size of hand
+			//printf("hand count: %d\n", handCount);
+			G.handCount[i] = handCount;					// assign to handCount	
+			//printf("G.handCount[i] is %d\n", G.handCount[i]);
+			for(j = 0; j < handCount; j++){ 			// handCount is set, iterate thru and assign randoms 
 				G.hand[i][j] = floor(Random() * (treasure_map + 1));
 			}
 		}
 		
 		// Create a random deck
-		for(i = 0; i < G.numPlayers; i++){
-			remCards = MAX_DECK - G.handCount[i]; 
-			deckCount = floor(Random() * remCards);	
-			G.deckCount[i] = deckCount; 
-			for(j = 0; j < deckCount; j++){
+		for(i = 0; i < G.numPlayers; i++){				
+			remCards = MAX_DECK - G.handCount[i]; 		// maximum possible cards left for deck
+			deckCount = floor(Random() * remCards);		// random number of cards in deck 0 to remCards
+			G.deckCount[i] = deckCount; 				// set deck size for player i
+			for(j = 0; j < deckCount; j++){				//for each card in deck, select random card
 				G.deck[i][j] = floor(Random() * (treasure_map + 1));
+				//printf("deck: %i, %i\n", i, G.deck[i][j]);
 			}
 		}
 		
@@ -68,57 +75,56 @@ int main() {
 	//cardEffect(smithy, 0, 0, 0, &testG, handPos, 0);
 	playCard((G.handCount[thisPlayer] - 1), 0, 0, 0, &G);
 	
-	printf ("----------Testing the Smithy card's effect on current player----------\n");
+	/*---------------------------------------------------------*/
+	// Testing the Smithy card's effect on current player
 
 	// Add 3 cards to current player's hand (since smithy is discarded, only 2 are actually added)
 	printf("hand count = %d   expected = %d\n", testG.handCount[thisPlayer], G.deckCount[thisPlayer] - 2);
     if(testG.handCount[thisPlayer] != G.handCount[thisPlayer] + 2){ 
-		printf("Failure: hand count did not increase by 3.\n");
+		printf("FAIL: hand count did not increase by 3.\n");
 	}
 	
 	// Take 3 cards from current player's pile
 	printf("deck count = %d   expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - 3);
     if(G.deckCount[thisPlayer] - 3 != testG.deckCount[thisPlayer]){  
-        printf("Failure: deck count did not decrease by 3.\n");
+        printf("FAIL: deck count did not decrease by 3.\n");
 	}
 	
 	// Current player's discard pile should be unaltered
 	printf("discard count = %d   expected = %d\n", testG.discardCount[thisPlayer], G.discardCount[thisPlayer] - 3);
     if(G.discardCount[thisPlayer] - 3 != testG.discardCount[thisPlayer]){  
-        printf("Failure: deck count did not decrease by 3.\n");
+        printf("FAIL: deck count did not decrease by 3.\n");
 	}
 	/*---------------------------------------------------------*/
 	
-	printf ("\n----------Testing the Smithy card's effect on player 2:----------\n");
+	// Testing the Smithy card's effect on the other players
 	
 	// Checks whether player 1's Smithy card affected player 2's hand count, deck count, or discard count.
-    printf("hand count = %d   expected = %d\n", testG.handCount[secPlayer], G.handCount[secPlayer]);
-	if(testG.handCount[secPlayer] != G.handCount[secPlayer]){
-		printf("Failure: player 1's smithy card affected player 2's hand count.\n");
-	}
-
-    printf("deck count = %d   expected = %d\n", testG.deckCount[secPlayer], G.deckCount[secPlayer]);
-	if(testG.deckCount[secPlayer] != G.deckCount[secPlayer]){
-		printf("Failure: player 1's smithy card affected player 2's deck count.\n");
-    }
-	
-	printf("discard count = %d   expected = %d\n", testG.discardCount[secPlayer], G.discardCount[secPlayer]);
-	if(testG.discardCount[secPlayer] != G.discardCount[secPlayer]){
-		printf("Failure: player 1's smithy card affected player 2's discard count.\n");
-    }
+		for(i = 0; i < G.numPlayers; i++){
+			if(i != thisPlayer){
+				if(testG.handCount[i] != G.handCount[i]){
+					printf("FAIL: Hand count mismatch for player %d. Count = %d   expected = %d\n", i, testG.handCount[i], G.handCount[i]);
+				}
+				if(testG.deckCount[i] != G.deckCount[i]){
+					printf("FAIL: Deck count mismatch for player %d. Count = %d   expected = %d\n", i, testG.deckCount[i], G.deckCount[i]);
+				}
+				if(testG.discardCount[i] != G.discardCount[i]){
+					printf("FAIL: Discard count mismatch for player %d. Count = %d   expected = %d\n", i, testG.discardCount[i], G.discardCount[i]);
+				}
+			}
+		}
 	/*---------------------------------------------------------*/
 
-	printf("\n----------Testing the Smithy card's effect on Victory and Kingdom piles:----------\n");
+	//Testing the Smithy card's effect on Victory and Kingdom piles
 	int arraySize = sizeof(G.supplyCount) / sizeof(int);
 	int i = 0;
 	for(i = 0; i < arraySize; i++){
 		printf("supply count = %d, expected %d\n", testG.supplyCount[i], G.supplyCount[i]);
 		if(testG.supplyCount[i] != G.supplyCount[i]){
-			printf("Failure: The supply of Victory and Kingdom cards was modified.\n");
+			printf("FAIL: The supply of Victory and Kingdom cards was modified.\n");
 		}
-	}
-	
+	}	
 	printf("\n ---Smithy card's testing is complete--- \n\n");
-	return 0;
 	}
+	return 0;
 }
