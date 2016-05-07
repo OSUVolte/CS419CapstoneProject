0030a8d9dd1351ecb4028c9c11639707946d1548
Michael Stramel <stramelm@flip2.engr.oregonstate.edu>
2016-05-07 08:15:00 -0700

    Cleaned up randomtestcard code

--- projects/stramelm/dominion/randomtestcard.c
+++ projects/stramelm/dominion/randomtestcard.c
@@ -7,7 +7,20 @@
 #include <string.h>
 #include <time.h>
 
+void print_mem(void const *vp, size_t n)
+{
+  // http://stackoverflow.com/questions/5349896/print-a-struct-in-c
+  unsigned char const *p = vp;
+  size_t i;
+  for (i=0; i<n; i++) {
+    printf("%02x%s", p[i],
+      i==0 ? "" : i % 16 == 15 ? "\n" : i % 4 == 3 ? " " : "");
+  }
+  putchar('\n');
+};
+
 int main(int argc, char *argv[]) {
+
   printf("CARD TEST #1: SMITHY\n\n");
 
   srand(time(NULL));
@@ -19,12 +32,12 @@ int main(int argc, char *argv[]) {
   int np, pn, hp; // number of players, player number, hand position
   int i, j, changed, seed;
   int r, calls = 0; // result of test smithy, number of calls to smithy
-  int pass[] = {0, 0, 0, 0, 0, 0}, fail[] = {0, 0, 0, 0, 0, 0}, t; // count of tests
+  int pass[] = {0, 0, 0, 0, 0, 0}, fail[] = {0, 0, 0, 0, 0, 0}, t; // test cnt
   int before, after; // ints used to assess pass/fail criteria
 
-  for (i = 0; i < 100; i++) { // set up 100 different sets of input conditions to test
+  for (i = 0; i < 100; i++) { // 100 different sets of input conds to test
     t = -1;
-    np = rand() % 3 + 2; // random number of players between 2 and 4
+    np = rand() % (MAX_PLAYERS - 1)  + 2; // random number players
     seed = rand() % 100 + 1; // random seed value between 1 and 100
 
     // create, initialize, and copy game state
@@ -41,82 +54,104 @@ int main(int argc, char *argv[]) {
     r = smithyCardEffect(pn, hp, state);
     calls++;
 
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     // REQT #1: FUNCTION SUCCESSFULLY COMPLETES
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     t++;
     if (r == 0) { pass[t]++; } else {
-		fail[t]++;
-		if (fail[t] == 1) { // first failure of this test
-          printf("Inputs that caused first failure of ... FUNCTION SUCCESSFULLY COMPLETES\n");
-		  printf("pn=%d\n", pn);
-		  printf("hp=%d\n\n", hp);
-		}
-	}
+      fail[t]++;
+      if (fail[t] == 1) { // first failure of this test
+        printf("Inputs that caused first failure of ...\n");
+        printf("FUNCTION SUCCESSFULLY COMPLETES\n");
+        printf("pn=%d\n", pn);
+        printf("hp=%d\n", hp);
+        printf("state=\n");
+        print_mem(state, sizeof(*state));
+      }
+    }
 
-    // -------------------------------------------------------------------------------------
-    // REQT #2: CURRENT PLAYER SHOULD REC'V 3 CARDS, BUT SMITHY TO BE DISCARDED, NET 2 IN HAND
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+    // REQT #2: CURRENT PLAYER SHOULD REC'V 3 CARDS,
+    // BUT SMITHY TO BE DISCARDED, NET 2 IN HAND
+    // -------------------------------------------------------------------------
     before = copy->handCount[pn];
     after = state->handCount[pn];
     t++;
     if (after - before == 2) { pass[t]++; } else {
-		fail[t]++;
-		if (fail[t] == 1) { // first failure of this test
-          printf("Inputs that caused first failure of ... CURRENT PLAYER SHOULD REC'V 3 CARDS, BUT SMITHY TO BE DISCARDED, NET 2 IN HAND\n");
-		  printf("pn=%d\n", pn);
-		  printf("hp=%d\n\n", hp);
-		}
-	}
+      fail[t]++;
+      if (fail[t] == 1) { // first failure of this test
+        printf("Inputs that caused first failure of ...\n");
+        printf("CURRENT PLAYER SHOULD REC'V 3 CARDS,\n");
+        printf("BUT SMITHY TO BE DISCARDED, NET 2 IN HAND\n");
+        printf("pn=%d\n", pn);
+        printf("hp=%d\n", hp);
+        printf("state=\n");
+        print_mem(state, sizeof(*state));
+      }
+    }
 
-    // -------------------------------------------------------------------------------------
-    // REQT #3: 3 CARDS SHOULD COME FROM HIS OWN PILE, BUT SMITHY WILL BE ADDED BACK TO THE DISCARD, NET -2
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+    // REQT #3: 3 CARDS SHOULD COME FROM HIS OWN PILE,
+    // BUT SMITHY WILL BE ADDED BACK TO THE DISCARD, NET -2
+    // -------------------------------------------------------------------------
     before = copy->discardCount[pn] + copy->deckCount[pn];
     after = state->discardCount[pn] + state->deckCount[pn];
     t++;
     if (after - before == 2) { pass[t]++; } else {
-		fail[t]++;
-		if (fail[t] == 1) { // first failure of this test
-          printf("Inputs that caused first failure of ... 3 CARDS SHOULD COME FROM HIS OWN PILE, BUT SMITHY WILL BE ADDED BACK TO THE DISCARD, NET -2\n");
-		  printf("pn=%d\n", pn);
-		  printf("hp=%d\n\n", hp);
-		}
-	}
+      fail[t]++;
+      if (fail[t] == 1) { // first failure of this test
+        printf("Inputs that caused first failure of ...\n");
+        printf("CARDS SHOULD COME FROM HIS OWN PILE,\n");
+        printf("BUT SMITHY WILL BE ADDED BACK TO THE DISCARD, NET -2\n");
+        printf("pn=%d\n", pn);
+        printf("hp=%d\n", hp);
+        printf("state=\n");
+        print_mem(state, sizeof(*state));
+      }
+    }
 
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     // REQT #4: NO STATE CHANGE FOR OTHER PLAYER(S)
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     t++;
-    for (j = 1; j <= np; j++) { // is this case j represents other player indices
-	  if (j == pn) { continue; } // skip is we are on the current player
+    for (j = 1; j <= np; j++) { // j represents other player indices
+
+      if (j == pn) { continue; } // skip is we are on the current player
 
       before = copy->handCount[j];
       after = state->handCount[j];
+
       if (after == before) { pass[t]++; } else {
-		fail[t]++;
-		if (fail[t] == 1) { // first failure of this test
-          printf("Inputs that caused first failure of ... NO STATE CHANGE FOR OTHER PLAYER(S)\n");
-		  printf("pn=%d\n", pn);
-		  printf("hp=%d\n\n", hp);
-		  }
-	  }
+        fail[t]++;
+        if (fail[t] == 1) { // first failure of this test
+          printf("Inputs that caused first failure of ...\n");
+          printf("NO STATE CHANGE FOR OTHER PLAYER(S)\n");
+          printf("pn=%d\n", pn);
+          printf("hp=%d\n", hp);
+          printf("state=\n");
+          print_mem(state, sizeof(*state));
+        }
+      }
 
       before = copy->discardCount[j] + copy->deckCount[j];
       after = state->discardCount[j] + state->deckCount[j];
+
       if (after == before) { pass[t]++; } else {
-		fail[t]++;
-		if (fail[t] == 1) { // first failure of this test
-          printf("Inputs that caused first failure of ... NO STATE CHANGE FOR OTHER PLAYER(S)\n");
-		  printf("pn=%d\n", pn);
-		  printf("hp=%d\n\n", hp);
-		  }
-	  }
+        fail[t]++;
+        if (fail[t] == 1) { // first failure of this test
+          printf("Inputs that caused first failure of ...\n");
+          printf("NO STATE CHANGE FOR OTHER PLAYER(S)\n");
+          printf("pn=%d\n", pn);
+          printf("hp=%d\n", hp);
+          printf("state=\n");
+          print_mem(state, sizeof(*state));
+        }
+      }
     }
 
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     // REQT #5: NO STATE CHANGE FOR VICTORY CARD PILE
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     changed = -1; // false = -1, true = 0
     for (j = 1; j <= 3; j++) {
       before = copy->supplyCount[j];
@@ -128,17 +163,20 @@ int main(int argc, char *argv[]) {
     }
     t++;
     if (changed == -1) { pass[t]++; } else {
-		fail[t]++;
-		if (fail[t] == 1) { // first failure of this test
-          printf("Inputs that caused first failure of ... NO STATE CHANGE FOR VICTORY CARD PILE\n");
-		  printf("pn=%d\n", pn);
-		  printf("hp=%d\n\n", hp);
-		}
-	}
+      fail[t]++;
+      if (fail[t] == 1) { // first failure of this test
+        printf("Inputs that caused first failure of ...\n");
+        printf("NO STATE CHANGE FOR VICTORY CARD PILE\n");
+        printf("pn=%d\n", pn);
+        printf("hp=%d\n", hp);
+        printf("state=\n");
+        print_mem(state, sizeof(*state));
+      }
+    }
 
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     // REQT #6: NO STATE CHANGE FOR KINGDOM CARD PILE
-    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     changed = -1; // false = -1, true = 0
     for (j = 7; j <= 26; j++) {
       before = copy->supplyCount[j];
@@ -150,22 +188,27 @@ int main(int argc, char *argv[]) {
     }
     t++;
     if (changed == -1) { pass[t]++; } else {
-		fail[t]++;
-		if (fail[t] == 1) { // first failure of this test
-          printf("Inputs that caused first failure of ... NO STATE CHANGE FOR KINGDOM CARD PILE\n");
-		  printf("pn=%d\n", pn);
-		  printf("hp=%d\n\n", hp);
-		}
-	}
+      fail[t]++;
+      if (fail[t] == 1) { // first failure of this test
+        printf("Inputs that caused first failure of ...\n");
+        printf("NO STATE CHANGE FOR KINGDOM CARD PILE\n");
+        printf("pn=%d\n", pn);
+        printf("hp=%d\n", hp);
+        printf("state=\n");
+        print_mem(state, sizeof(*state));
+      }
+    }
 
     // clean up
     free(state);
     free(copy);
 
-}
+  }
+
+  // print summary reports from tests that were run
 
   t = -1;
-  printf("TOTAL CALLS TO smithyCardEffect(): %d\n\n", calls);
+  printf("\nTOTAL CALLS TO smithyCardEffect(): %d\n\n", calls);
 
   t++;
   printf("FUNCTION SUCCESSFULLY COMPLETES\n");
@@ -174,13 +217,15 @@ int main(int argc, char *argv[]) {
   printf("Fail: %d\n\n", fail[t]);
 
   t++;
-  printf("CURRENT PLAYER SHOULD REC'V 3 CARDS, BUT SMITHY TO BE DISCARDED, NET 2 IN HAND\n");
+  printf("CURRENT PLAYER SHOULD REC'V 3 CARDS,\n");
+  printf("BUT SMITHY TO BE DISCARDED, NET 2 IN HAND\n");
   printf("-------------------------------\n");
   printf("Pass: %d\n", pass[t]);
   printf("Fail: %d\n\n", fail[t]);
 
   t++;
-  printf("3 CARDS SHOULD COME FROM HIS OWN PILE, BUT SMITHY WILL BE ADDED BACK TO THE DISCARD, NET -2\n");
+  printf("3 CARDS SHOULD COME FROM HIS OWN PILE,\n");
+  printf("BUT SMITHY WILL BE ADDED BACK TO THE DISCARD, NET -2\n");
   printf("-------------------------------\n");
   printf("Pass: %d\n", pass[t]);
   printf("Fail: %d\n\n", fail[t]);
@@ -203,9 +248,7 @@ int main(int argc, char *argv[]) {
   printf("Pass: %d\n", pass[t]);
   printf("Fail: %d\n\n", fail[t]);
 
-  for (i = 0; i < 6; i++) {
-	  printf("pass = %d, fail = %d\n", pass[i], fail[i]);
-
-}
   return 0;
 }
+
+
