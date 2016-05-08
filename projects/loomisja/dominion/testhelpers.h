fc2b3c7d1baf11d3758779cdeb899c5fdbcdfefd
Jason Loomis <loomisja@onid.oregonstate.edu>
2016-05-07 23:28:44 -0700

    Assignment 4 Submission

--- projects/loomisja/dominion/testhelpers.h
+++ projects/loomisja/dominion/testhelpers.h
@@ -4,8 +4,8 @@
 /*******************************************************************************
 ** Program Filename: testhelpers.h
 ** Author: Jason Loomis
-** Date: 4/24/16
-** Description: CS 362, Assignment 3
+** Date: 5/8/16
+** Description: CS 362, Assignment 4
 **		Header for helper library for testing Dominion game.
 **		Contains functions to assess game/player state, card counts, etc.
 *******************************************************************************/
@@ -18,7 +18,7 @@
  * 1 = summary output
  * 2 = all test results
  */
-#define OUTPUTLEVEL 1
+#define OUTPUTLEVEL 2
 
 #define FALSE 0
 #define TRUE 1
@@ -35,6 +35,42 @@
 
 /* A standard set of kingdom cards, the first 10 defined in cardEffect() */
 extern int Kingdom[];
+
+/*******************************************************************************
+** Function: rand32
+** Description: bitwise combine multiple 16-bit random values into a single
+**		positive 32-bit random integer
+** Parameters: None
+** Pre-Conditions:
+**		srand() has been called to initialize the random number generator
+** Post-Conditions: returns a random positive 32-bit integer value
+*******************************************************************************/
+int rand32();
+
+/*******************************************************************************
+** Function: randU32
+** Description: bitwise combine multiple 16-bit random values into a single
+**		32-bit random unsigned integer
+** Parameters: None
+** Pre-Conditions:
+**		srand() has been called to initialize the random number generator
+** Post-Conditions: returns a random 32-bit unsigned integer value
+*******************************************************************************/
+unsigned int randU32();
+
+/*******************************************************************************
+** Function: chooseRandomCard
+** Description: choose a card randomly from among the kingdom cards, treasure,
+**		victory, or curse
+** Parameters:
+**		kingdom: pointer to array of kingdom cards
+** Pre-Conditions:
+**		srand() has been called to initialize the random number generator;
+**		kingdom points to an array of 10 integers, all of which are valid
+**			card types from the CARD enumeration
+** Post-Conditions: the chosen random card type is returned
+*******************************************************************************/
+int chooseRandomCard(int* kingdom);
 
 /*******************************************************************************
 ** Function: initCounts
@@ -67,6 +103,17 @@ void copyGameState(struct gameState* dest, struct gameState* src);
 ** Post-Conditions: returns FALSE if bytes are equal, otherwise TRUE
 *******************************************************************************/
 int gameStateChanged(struct gameState* a, struct gameState* b);
+
+/*******************************************************************************
+** Function: isValidCardType
+** Description: check if a card type value is a value from the CARD enumeration.
+** Parameters:
+**		card: a card type value
+** Pre-Conditions: None
+** Post-Conditions: returns TRUE if the value is within the CARD enumeration,
+**		otherwise FALSE
+*******************************************************************************/
+int isValidCardType(int card);
 
 /*******************************************************************************
 ** Function: deltaPlayerHandCount
@@ -502,6 +549,17 @@ int checkSpecificPlayedCard(struct gameState* before, struct  gameState* after, 
 void updateTestResult(int result, int* passes, int* failures);
 
 /*******************************************************************************
+** Function: printTestResult
+** Description: print the test result (pass/fail) to stdout.
+** Parameters:
+**		result: boolean result of the test, zero for failure, otherwise success
+** Pre-Conditions: None
+** Post-Conditions: if result is non-zero, "passed" message printed to stdout;
+**		otherwise, "failed" message is printed to stdout.
+*******************************************************************************/
+void printTestResult(int result);
+
+/*******************************************************************************
 ** Function: printIndent
 ** Description: print the specified indentation.
 ** Parameters:
@@ -510,5 +568,53 @@ void updateTestResult(int result, int* passes, int* failures);
 ** Post-Conditions: the specified number of indentations are printed to stdout.
 *******************************************************************************/
 void printIndent(int indent);
+
+/*******************************************************************************
+** Function: randomizeGameState
+** Description: initialize a randomized game state
+** Parameters:
+**		state: pointer to a gameState
+**		kingdom: pointer to array of kingdom cards
+**		cardToPlay: the specific card type to be played
+**		handpos: the hand position at which cardToPlay is located for the
+**			player specified by gameState.whoseTurn
+** Pre-Conditions:
+**		srand() has been called to initialize the random number generator;
+**		state is a pointer to a valid gameState struct;
+**		kingdom points to an array of 10 integers, all of which are valid
+**			card types from the CARD enumeration
+**		cardToPlay should be a valid card type from the CARD enumeration,
+**			additionally, cardToPlay should be one of the kingdom card types
+** Post-Conditions: initializes the gameState using the provided kingdom cards,
+**		the parameters are randomized as follows:
+**			numPlayers: 2 <= numPlayers <= MAX_PLAYERS
+**			supplyCount: all kingdom, treasure, victory, curse cards between 0
+**				and game initial counts
+**			embargo: if embargo is in the kingdom, embargo tokens may be
+**				may be applied to any kingdom supply piles; the number of
+**				tokens applied is up to (10 - # embargoes remaining in supply)
+**			outpostPlayed: if outpost is in the kingdom, with a 1/32 chance,
+**				outpostPlayed flag is set to up to (10 - # outposts remaining
+**				in supply)
+**			outpostTurn: if the outpostPlayed flag is non-zero, outpostTurn
+**				is set to a value between 0 and 2.
+**			whoseTurn: a player between 0 and (numPlayers - 1) is selected
+**			numActions: random value between 0 and 10 (arbitrary)
+**			coins: random value between 0 and 20 (arbitrary)
+**			numBuys: random value between 0 and 10 (arbitrary)
+**			cards counts: a cardcount is selected, between 0 and MAX_HAND,
+**				this value is then randomly divided among the deck, hand, and
+**				discard for each player; for the current player (whoseTurn),
+**				the distribution includes the played cards as well.
+**			cards: for the count of cards specified, the deck, hand, and
+**				discard are chosen randomly from among the kingdom, treasure
+**				victory, and curse cards; for the current player (whoseTurn),
+**				the playedCards are chosen as well.
+**			cardToPlay + handpos: for the current player (whoseTurn), the
+**				player's hand is specified to have at least 1 card and that
+**				card is specified to be cardToPlay and is set at any valid
+**				handpos for that player's hand.
+*******************************************************************************/
+void randomizeGameState(struct gameState* state, int* kingdom, int cardToPlay, int* handpos);
 
 #endif
