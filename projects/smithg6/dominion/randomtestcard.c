368e638ad87cf12616020cdf975f9199680045a1
smithg6 <smithg6@oregonstate.edu>
2016-05-08 16:28:59 -0700

    Added ProjectPartA.txt

---
+++ projects/smithg6/dominion/randomtestcard.c
@@ -0,0 +1,23 @@
+/*
+----------------------------------------------------
+Grant Smith
+CS362
+Assignment 4
+This is a random test generator to test the functionality of the
+village card. It will randomly generate a game state to input into
+the cardAdventurer function and compare expected results with actual
+results and report unexpected failures.
+----------------------------------------------------
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <time.h>
+#include <limits.h>
