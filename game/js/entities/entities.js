81d291a8756daeb1f6e983abd83df6068d02dc2a
prophet60091 <jackrobe@onid.oregonstate.edu>
2016-11-04 09:54:33 -0600

    fixes error on unit spawn

--- game/js/entities/entities.js
+++ game/js/entities/entities.js
@@ -689,10 +689,10 @@ function Unit(hp, def, atk, speed, hitPercent, dodge, type, buildTime, name, ima
 }
 
 
-// Unit(hp, def, atk, speed, hitPercent, dodge, type, name, image)
-var slime = new Unit(10, 1, 3, 5, 90, 3, 1, "Slime", "slime spritesheet calciumtrice_0");
-var rogue = new Unit(15, 2, 10, 10, 98, 30, 2, "Rogue", "rogue spritesheet calciumtrice");
-var warrior = new Unit(10, 4, 5, 3, 70, 3, 3, "Warrior", "warrior spritesheet calciumtrice");
+// Unit(hp, def, atk, speed, hitPercent, dodge, type, buildTime, name, image)
+var slime = new Unit(10, 1, 3, 5, 90, 3, 1, 10, "Slime", "slime spritesheet calciumtrice_0");
+var rogue = new Unit(15, 2, 10, 10, 98, 30, 2, 10, "Rogue", "rogue spritesheet calciumtrice");
+var warrior = new Unit(10, 4, 5, 3, 70, 3, 3, 10, "Warrior", "warrior spritesheet calciumtrice");
 
 
 
