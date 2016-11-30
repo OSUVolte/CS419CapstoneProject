62dfd7a5e9b26f48ae85652d844e9c9fb04253b9
Robert Jackson <jackrobe@oregonstate.edu>
2016-11-29 23:38:05 -0700

    merged

---
+++ game/js/entities/GameConditionals.js
@@ -0,0 +1,28 @@
+/**
+ * Created by rjack on 11/19/2016.
+ */
+
+me.data.conditionals = {
+
+    //time at which q one will be released
+    waveTime : function(){
+
+    },
+    /**
+     * BUILDING RELATED
+     */
+    //When will  building of type2 (rogue) entities be allowed
+    //when player has x units in play, and gold greater than unit cost
+    type2Allowed : function(){
+
+    },
+    //When will  building of type3 (slime) entities be allowed
+    //when player has x units in play, and gold greater than unit cost
+    type2Allowed : function(){
+
+    },
+
+
+
+
+}
