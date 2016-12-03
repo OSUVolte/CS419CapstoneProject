4121fe0dbdcfbcf56dcad91e234c52581d603ec5
John Lewis Kilgore <jlewis.kilgore@gmail.com>
2016-12-02 20:00:50 -0500

    reset game.data and game.dataAI properties on a continued play

--- game/js/screens/play.js
+++ game/js/screens/play.js
@@ -6,10 +6,44 @@ game.PlayScreen = me.ScreenObject.extend({
         // load level
         me.levelDirector.loadLevel("test_map3-fakeIso");
 
-        // reset the score
+        // reset the game data for keeping track of time a waves
         game.data.gametime = 0;
-        game.data.gametimeoffset = me.timer.getTime();
+        game.data.gametimeoffset = me.timer.getTime(); //factor out time spent in title screen, game over screen, and previous games
         game.data.lastAIspawnTime = 0;
+        game.data.currentwave = 0;
+        game.data.playergold = 10000;
+        game.data.player1win = 0;
+
+        game.dataAI.gametime = 0;
+        game.dataAI.currentwave = 0;
+        game.data.playergold = 1000;
+
+        // reset upgrades
+        // PLAYER
+        game.data.atkBoost = 0; //attack boost
+        game.data.defBoost = 0; // defence boost
+        game.data.btBoost = 0; //buildtime Boost
+        game.data.hpBoost = 0; //hp boost
+        game.data.speedBoost = 0;
+        game.data.dodgeBoost = 0;
+        game.data.sfArmor = 1;
+        game.data.sfSpeed = 1;
+        game.data.sfHealth = 1;
+        game.data.sfAtk = 1;
+        game.data.sfBuildTime = 1;
+        // AI
+        game.dataAI.atkBoost = 0; //attack boost
+        game.dataAI.defBoost = 0; // defence boost
+        game.dataAI.btBoost = 0; //buildtime Boost
+        game.dataAI.hpBoost = 0; //hp boost
+        game.dataAI.speedBoost = 0;
+        game.dataAI.dodgeBoost = 0;
+        game.dataAI.sfArmor = 1;
+        game.dataAI.sfSpeed = 1;
+        game.dataAI.sfHealth = 1;
+        game.dataAI.sfAtk = 1;
+        game.dataAI.sfBuildTime = 1;
+
         //Build menu
         //todo add a map object then we wont need this...I think?
         game.data.buildbutton = me.pool.pull("buildbutton", 10, 10, {});
